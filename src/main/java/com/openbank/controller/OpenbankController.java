156c70f42d61214aa122a584b4967e1b726cc2c1
Rico <javier.rico@accenture.com>
2023-03-01 12:17:38 +0100

    add mapping

--- src/main/java/com/openbank/controller/OpenbankController.java
+++ src/main/java/com/openbank/controller/OpenbankController.java
@@ -1,34 +1,29 @@
 package com.openbank.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.openbank.jdbc.Transactions;
 
 @RestController
 @RequestMapping("test")
 public class OpenbankController {
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
+//    @Autowired
+//    JdbcTemplate jdbcTemplate;
+//
+//    @GetMapping("/transactions")
+//    public @ResponseBody ResponseEntity<String> transactions() {
+//        List<String> list = new ArrayList<>();
+//        list.add("Table data...");
+//        jdbcTemplate.query(
+//                "SELECT * FROM transactions", new Object[]{},
+//                (rs,rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"), rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")))
+//                .forEach(thing -> list.add(thing.toString()));
+//        return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
+//    }
     @GetMapping("/hello")
-    public @ResponseBody ResponseEntity<String> transactions() {
-        List<String> list = new ArrayList<>();
-        list.add("Table data...");
-        jdbcTemplate.query(
-                "SELECT * FROM transactions", new Object[]{},
-                (rs,rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"), rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")))
-                .forEach(thing -> list.add(thing.toString()));
-        return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
