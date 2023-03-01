29809e3eb6fb699b05c453cd26a09fcb362f8bec
Rico <javier.rico@accenture.com>
2023-03-01 12:26:11 +0100

    add mapping

--- src/main/java/com/openbank/openbank/OpenbankApplication.java
+++ src/main/java/com/openbank/openbank/OpenbankApplication.java
@@ -3,7 +3,7 @@ package com.openbank.openbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.openbank.controller"})
 public class OpenbankApplication {
 
 	public static void main(String[] args) {
