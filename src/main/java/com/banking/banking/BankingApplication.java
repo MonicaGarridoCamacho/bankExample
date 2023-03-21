312b6ae8863ed839a5cf71e62711415913fe06db
Monica <monica.garrido@accenture.com>
2023-03-21 14:09:35 +0100

    JSON created

--- src/main/java/com/banking/banking/BankingApplication.java
+++ src/main/java/com/banking/banking/BankingApplication.java
@@ -2,8 +2,9 @@ package com.banking.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.banking.controller"})
+@SpringBootApplication(scanBasePackages = {"com.banking.controller"}, exclude = {JdbcTemplateAutoConfiguration.class})
 public class BankingApplication {
 
 	public static void main(String[] args) {
