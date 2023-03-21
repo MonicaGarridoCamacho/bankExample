19b67994798d3c7a8119a0de51468319d8a0e726
Monica <monica.garrido@accenture.com>
2023-03-21 12:03:48 +0100

    JSON created

--- src/main/java/com/banking/controller/BankingController.java
+++ src/main/java/com/banking/controller/BankingController.java
@@ -3,9 +3,8 @@ package com.banking.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,39 +23,44 @@ public class BankingController {
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/transactions")
-	public JSONArray transactions() {
+	public @ResponseBody ResponseEntity<String> transactions() {
 		List<String> list = new ArrayList<>();
-		/*jdbcTemplate.execute(
-				"select json_object ('accountId' value accountId,'transactionId' value transactionId,'transactionInformation' value transactionInformation,'addressLine' value addressLine,'amount' value amount)from transactions;");
-		
-		 */
-		jdbcTemplate.query("select json_object ('accountId' value accountId,                    'transactionId' value transactionId,                    'transactionInformation' value transactionInformation,                    'addressLine' value addressLine,                    'amount' value amount                     absent on null)from transactions;", new Object[] {}, (rs,
-		 rowNum) -> new Transactions(rs.getString("accountId"),
-		 rs.getString("transactionId"), rs.getString("transactionInformation"),
-		 rs.getString("addressLine"), rs.getString("amount"))) .forEach(thing ->
-		 list.add(thing.toString()));
-		 JSONArray ja = new JSONArray(list);
-		 return ja;
-	}
+		jdbcTemplate.query("SELECT * FROM transactions", new Object[] {},
+				(rs, rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"),
+						rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")))
+				.forEach(thing -> list.add(thing.toString()));
+		//return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
+		//This part is not working.
+		System.out.println("listMoni : " + list);
+	    /*try {
+	        JSONObject arr = new JSONObject((list));
+	        for (int i = 0; i < arr.length(); i++) {
+	          JSONObject abc = arr.getJSONObject(arr.get(i));
+	          System.out.println("test1 : " + abc.getString("test1"));
+	          System.out.println("test2 : " + abc.getString("test2"));
+	          System.out.println("test3 : " + abc.getString("test3"));
+	        }
+	    } catch (Exception e) {
+	    }*/
 
+	    return result;
+	}
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/hello")
 	public String hello(@RequestParam(name = "name", defaultValue = "World") String name) {
-		return "Hello " + name;
+	    return "Hello " + name;
 	}
-
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/accounts")
-	public Transactions transactionsId(@RequestParam(name = "accountId", defaultValue = "0") String accountId) {
-		List<String> list = new ArrayList<>();
-		list.add("Table data...");
-		return jdbcTemplate.queryForObject("SELECT * FROM transactions WHERE accountId=?", new Object[] { accountId },
-				(rs, rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"),
-						rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")));
+    public Transactions transactionsId(@RequestParam(name = "accountId", defaultValue = "0") String accountId) {
+        List<String> list = new ArrayList<>();
+        list.add("Table data...");
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM transactions WHERE accountId=?", new Object[]{ accountId },
+                (rs,rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"), rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")));
 
-	}
+    }
 
 }
