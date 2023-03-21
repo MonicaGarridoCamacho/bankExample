379f8856a29e100efe90a652910484e49a7d5020
Monica <monica.garrido@accenture.com>
2023-03-21 14:18:52 +0100

    JSON created

--- src/main/java/com/banking/controller/BankingController.java
+++ src/main/java/com/banking/controller/BankingController.java
@@ -3,7 +3,6 @@ package com.banking.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,52 +26,12 @@ public class BankingController {
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/transactions")
 	public @ResponseBody ResponseEntity<String> transactions() {
-		/*List<Transactions> list = new ArrayList<Transactions>();
-		List<JSONObject> listJSON = new ArrayList<JSONObject>();
+		List<String> list = new ArrayList<>();
 		jdbcTemplate.query("SELECT * FROM transactions", new Object[] {},
 				(rs, rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"),
 						rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")))
-				.forEach(thing -> list.add(0, thing));
-		 
-		System.out.println("list Moni " + list);
-	    //This part is not working.
-	    /*try {
-	    	
-	        for (Transactions tr : list) {
-	        	JSONObject json = new JSONObject();
-	        	json.put("accountId", tr.getAccountId());
-	        	json.put("transactionId", tr.getTransactionId());
-	        	json.put("transactionInformation", tr.getTransactionInformation());
-	        	json.put("addressLine", tr.getAddressLine());
-	        	json.put("amount", tr.getAmount());
-	        	listJSON.add(json);
-	        }
-	    } catch (Exception e) {
-	    }*/
-	   
-	   /* return ResponseEntity<String>(list.toString(), HttpStatus.OK);*/
-		List<Transactions> list = new ArrayList<Transactions>();
-		List<JSONObject> listJSON = new ArrayList<JSONObject>();
-		jdbcTemplate.query("SELECT * FROM transactions", new Object[] {},
-				(rs, rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"),
-						rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")))
-				.forEach(thing -> list.add(0, thing));
-		 
-		System.out.println("list Moni " + list);
-		try {
-	    	
-	        for (Transactions tr : list) {
-	        	JSONObject json = new JSONObject();
-	        	json.put("accountId", tr.getAccountId());
-	        	json.put("transactionId", tr.getTransactionId());
-	        	json.put("transactionInformation", tr.getTransactionInformation());
-	        	json.put("addressLine", tr.getAddressLine());
-	        	json.put("amount", tr.getAmount());
-	        	listJSON.add(json);
-	        }
-	    } catch (Exception e) {
-	    }
-		return new ResponseEntity<String>(listJSON.toString(), HttpStatus.OK); 
+				.forEach(thing -> list.add(thing.toString()));
+		return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
 	}
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/hello")
