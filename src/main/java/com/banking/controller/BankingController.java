f792330f21c8d3fe8a5328d9a2bd4a96bf7d6dc9
Monica <monica.garrido@accenture.com>
2023-03-21 12:29:23 +0100

    JSON created

--- src/main/java/com/banking/controller/BankingController.java
+++ src/main/java/com/banking/controller/BankingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -25,28 +26,31 @@ public class BankingController {
 	JdbcTemplate jdbcTemplate;
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/transactions")
-	public @ResponseBody JSONObject transactions() {
+	public @ResponseBody ResponseEntity<String> transactions() {
 		List<String> list = new ArrayList<>();
 		jdbcTemplate.query("SELECT * FROM transactions", new Object[] {},
 				(rs, rowNum) -> new Transactions(rs.getString("accountId"), rs.getString("transactionId"),
 						rs.getString("transactionInformation"), rs.getString("addressLine"), rs.getString("amount")))
 				.forEach(thing -> list.add(thing.toString()));
-		//return new ResponseEntity<String>(list.toString(), HttpStatus.OK);
-		//This part is not working.
-		System.out.println("listMoni : " + list);
-		System.out.println("arrayJSON : " + new JSONObject((list)));
-	    /*try {
-	        JSONObject arr = new JSONObject((list));
-	        for (int i = 0; i < arr.length(); i++) {
-	          JSONObject abc = arr.getJSONObject(arr.get(i));
+		 
+		
+		ResponseEntity<String> result = new ResponseEntity<String>(list.toString(), HttpStatus.OK);     
+	    System.out.println(result);
+
+	    //This part is not working.
+	    try {
+	        JSONObject arr = new JSONObject((result));
+	        System.out.println("moniiiiiiii " + arr);
+	        /*for (int i = 0; i < arr.length(); i++) {
+	          JSONObject abc = arr.getJSONObject(i);
 	          System.out.println("test1 : " + abc.getString("test1"));
 	          System.out.println("test2 : " + abc.getString("test2"));
 	          System.out.println("test3 : " + abc.getString("test3"));
-	        }
+	        }*/
 	    } catch (Exception e) {
-	    }*/
+	    }
 
-	    return new JSONObject((list));
+	    return result;
 	}
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/hello")
